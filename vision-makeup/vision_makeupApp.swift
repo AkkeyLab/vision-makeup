b356ca505e56acec6ca61f768a516c0012ae7e85
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-08-11 18:09:58 +0900

    :sparkles: Create project base files

---
+++ vision-makeup/vision_makeupApp.swift
@@ -0,0 +1,17 @@
+//
+//  vision_makeupApp.swift
+//  vision-makeup
+//
+//  Created by AkkeyLab on 2023/08/11.
+//
+
+import SwiftUI
+
+@main
+struct vision_makeupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
